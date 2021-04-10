59c6af92d30d9ef8dd0df8dd9f81308b5801bf9e
LA55 <omskathletics@mail.ru>
2021-04-10 18:57:53 +0600

    Cats images. Only big for now

--- site/components/category/CatList.js
+++ site/components/category/CatList.js
@@ -6,6 +6,9 @@ const CatList = ({ cats }) => (
         { cats.map(cat => (
             <Link key={ cat.id } href={`/catalog/${cat.id}`}>
                 <div className={styles.card}>
+                    <div className={styles.image}>
+                        <img src={`/media/catphoto/${cat.id}.jpg`} width="30px" height="30px"/>
+                    </div>
                     <div className={styles.title}>
                         { cat.title }
                     </div>
