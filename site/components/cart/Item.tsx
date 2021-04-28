e6b96f1c298cef2b68a17acd075febbc0e2a5c81
LA55 <omskathletics@mail.ru>
2021-04-28 20:20:55 +0600

    Cart items styling

--- site/components/cart/Item.tsx
+++ site/components/cart/Item.tsx
@@ -1,19 +1,37 @@
-import { useContext } from 'react'
+import { useState, useContext } from 'react'
 import { CartContext } from '../../context/cart/cart.provider'
+import styles from './Item.module.scss'
 
 const SingleItem = ({product, qty}) => {
+    const [image, setImage] = useState(`https://stroitel55.com/media/productphoto/${product.id}.jpg`)
     const { addItem, changeItemQty, removeItem } = useContext(CartContext)
 
     return (
-        <>
-            <div>{ product.title }</div>
-            <div>
-                <span onClick={() => removeItem(product.id)}>&larr;</span>
-                <input type="text" value={ qty }
-                 onChange={(e) => changeItemQty(product.id, e.target.value)}/>
-                <span onClick={() => addItem({ product, qty: 1 })}>&rarr;</span>
+        <div className={styles.card}>
+            <div className={styles.image}>
+                <img
+                    src={image}
+                    onError={() => setImage('/static/svg/loading.gif')}
+                    alt={ product.title }
+                />
             </div>
-        </>
+            <div className={styles.info}>
+                <div className={styles.pay}>
+                    <div className={styles.qty}>
+                        <span onClick={() => removeItem(product.id)}>&larr;</span>
+                        <input type="text" value={ qty }
+                        onChange={(e) => changeItemQty(product.id, e.target.value)}/>
+                        <span onClick={() => addItem({ product, qty: 1 })}>&rarr;</span>
+                    </div>
+                    <div className={styles.price}>
+                        { (parseFloat(product.price) * qty).toFixed(2) } руб.
+                    </div>
+                </div>
+                <div className={styles.title}>
+                    { product.title }
+                </div>
+            </div>
+        </div>
     )
 }
 
